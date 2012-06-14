e9ea0d36a5d8d2b96ae1f983d8250d82f4edbebd
projedi <projedi666@gmail.com>
2012-06-15 03:34:54 +0400

    Probably useless class Player

--- ffmpeg/player.cpp
+++ ffmpeg/player.cpp
@@ -1,1 +1,33 @@
 #include "player.h"
+
+Player::Player(int width, int height, QString audioOutput) {
+   QtConcurrent::run(this, &Player::init, width, height, audioOutput);
+}
+
+Player::~Player() {
+   //avcodec_close(audioCodec);
+   //avformat_free_context(audioFormat);
+   //TODO: Where will it SEGFAULT?
+   //av_free(audioCodec);
+   //av_free(audioFormat);
+}
+
+void Player::newVideoFrame(AVFrame* frame) {
+   QImage img(frame->data[0],frame->width,frame->height,QImage::Format_RGB32);
+   emit onNewFrame(QPixmap::fromImage(img));
+   //TODO: What to do with frame?
+}
+
+//TODO: Add audio output
+void Player::newAudioFrame(AVFrame* frame) {
+
+}
+
+void Player::init(int width, int height, QString audioOutput) {
+   QMutexLocker(&initLocker);
+   //TODO: Add audio output
+   //QByteArray audioName = audioOutput.toAscii();
+
+   this->width = width;
+   this->height = height;
+}
--- ffmpeg/player.h
+++ ffmpeg/player.h
@@ -2,21 +2,26 @@
 
 //TODO: It can't dynamically resized. Bad.
 //TODO: What if I want different audio renderings on different platforms? For some reason.
-class Player: public FFSink, public FFVideoDevice {
+//TODO: There's no native ffmpeg method to output audio on windows. Therefore,
+//TODO: this solution is just to display local video
+//TODO: All that in mind, this class is probably useless
+class Player: public FFSink {
    Q_OBJECT
 public:
    Player(int width, int height, QString audioOutput);
    ~Player();
    void newVideoFrame(AVFrame* frame);
    void newAudioFrame(AVFrame* frame);
-   int getWidth() { QMutexLocker(&initLocker); return codec->width; }
-   int getHeight() { QMutexLocker(&initLocker); return codec->height; }
-   PixelFormat getPixelFormat() { QMutexLocker(&initLocker); return codec->pix_fmt; }
+   int getWidth() { QMutexLocker(&initLocker); return width; }
+   int getHeight() { QMutexLocker(&initLocker); return height; }
+   PixelFormat getPixelFormat() { QMutexLocker(&initLocker); return PIX_FMT_RGB32; }
 signals:
    void onNewFrame(QPixmap frame);
 private:
-   AVFormatContext* audioFormat;
-   AVCodecContext* videoCodec;
-   AVCodecContext* audioCodec;
+   //AVFormatContext* audioFormat;
+   //AVCodecContext* audioCodec;
    QMutex initLocker;
+   void init(int width, int height, QString audioOutput);
+   int width;
+   int height;
 };
