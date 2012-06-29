283f832735bd79a776913005ef7fafdcf13311fe
Alexander Shabalin <shabalyn.a@gmail.com>
2012-06-29 17:06:16 +0400

    Client turned to the new interface

--- ffmpeg/client.cpp
+++ ffmpeg/client.cpp
@@ -1,21 +1,16 @@
 #include "client.h"
 
 Client::Client(QString file) {
-   QtConcurrent::run(this, &Client::init, file);
+   initFuture = QtConcurrent::run(this, &Client::init, file);
 }
 
 Client::~Client() {
-   avcodec_close(audioCodec);
-   avcodec_close(videoCodec);
-   avformat_free_context(format);
-   //TODO: Where will it SEGFAULT?
-   av_free(audioCodec);
-   av_free(videoCodec);
-   av_free(format);
+   avcodec_close(aCodec); delete aCodec;
+   avcodec_close(vCodec); delete vCodec;
+   avformat_free_context(format); delete format;
 }
 
 void Client::init(QString file) {
-   QMutexLocker(&initLocker);
    QByteArray fileName = file.toAscii();
 
    format = avformat_alloc_context();
@@ -26,19 +21,19 @@ void Client::init(QString file) {
    
    AVCodec* videoDecoder;
    //TODO: Handle error codes better
-   videoStreamIndex = av_find_best_stream(format,AVMEDIA_TYPE_VIDEO,-1,-1,&videoDecoder,0);
+   vIndex = av_find_best_stream(format,AVMEDIA_TYPE_VIDEO,-1,-1,&videoDecoder,0);
    if(!videoDecoder) cout << "Can't find video decoder for client" << endl;
-   videoCodec = videoFormat->streams[videoStreamIndex]->codec;
-   if(!videoCodec) cout << "Can't get video codec context for client" << endl;
-   if(avcodec_open2(videoCodec,videoDecoder,0) < 0)
+   vCodec = videoFormat->streams[vIndex]->codec;
+   if(!vCodec) cout << "Can't get video codec context for client" << endl;
+   if(avcodec_open2(vCodec,videoDecoder,0) < 0)
       cout << "Can't associate video codec on input for client" << endl;
 
-   AVCocec* audioCoder
-   audioStreamIndex = av_find_best_stream(format,AVMEDIA_TYPE_AUDIO,-1,-1,&audioDecoder,0);
+   AVCodec* audioCoder
+   aIndex = av_find_best_stream(format,AVMEDIA_TYPE_AUDIO,-1,-1,&audioDecoder,0);
    if(!audioDecoder) cout << "Can't find audio decoder for client" << endl;
-   audioCodec = audioFormat->streams[audioStreamIndex]->codec;
-   if(!audioCodec) cout << "Can't get audio codec context for client" << endl;
-   if(avcodec_open2(audioCodec,audioDecoder,0) < 0)
+   aCodec = audioFormat->streams[aIndex]->codec;
+   if(!aCodec) cout << "Can't get audio codec context for client" << endl;
+   if(avcodec_open2(aCodec,audioDecoder,0) < 0)
       cout << "Can't associate audio codec on input for client" << endl;
 
    QtConcurrent::run(this, &Client::worker);
@@ -46,22 +41,22 @@ void Client::init(QString file) {
 
 //TODO: Proper memory management
 void Client::worker() {
-   AVFrame* frame;
-   int pts = 0;
-   int frameFinished;
-   int got_frame;
    while(true) {
       AVPacket pkt;
       if(av_read_frame(videoFormat,&pkt) < 0) cout << "Can't read frame" << endl;
-      if(pkt.stream_index == videoStreamIndex) {
-         frame = avcodec_alloc_frame();
-         //TODO: technically I should check that frame is finished
-         if(avcodec_decode_video2(videoCodec,frame,&frameFinished,&pkt) < 0)
+      if(pkt.stream_index == vIndex) {
+         int got_picture;
+         AVFrame* frame = avcodec_alloc_frame();
+         if(avcodec_decode_video2(vCodec,frame,&got_picture,&pkt) < 0) {
             cout << "Can't decode pkt" << endl;
-         //TODO: proper pts handling
-         frame->pts = pts++;
-         emit onNewVideoFrame(frame);
+            continue;
+         }
+         if(!got_picture) continue;
+         frame->pts = av_frame_get_best_effort_timestamp(frame);
+         emit onNewVideoFrame(QAVFrame(frame));
+         av_free_packet(&pkt);
       } else if(pkt.stream_index == audioStreamIndex) {
+         /*
          while(pkt.size > 0) {
             frame = avcodec_alloc_frame();
             int len = avcodec_decode_audio4(audioCodec,frame,&got_frame,&pkt);
@@ -74,8 +69,7 @@ void Client::worker() {
             frame->pts = pts++;
             emit onNewAudioFrame(frame);
          }
+         */
       }
-      //TODO: Do I need to do that?
-      av_free_packet(&pkt);
    }
 }
--- ffmpeg/client.h
+++ ffmpeg/client.h
@@ -1,29 +1,26 @@
 #include "ffmpeg.h"
 
-//TODO: Client & Server? Seriously? WHY U NO USE PROPER NAMING?
+// Get stream from file or some network stream.
 class Client: public FFSource {
    Q_OBJECT
 public:
    Client(QString file);
    ~Client();
-   int getWidth() { QMutexLocker(&initLocker); return videoCodec->width; }
-   int getHeight() { QMutexLocker(&initLocker); return videoCodec->height; }
-   PixelFormat getPixelFormat() { QMutexLocker(&initLocker); return videoCodec->pix_fmt; }
-   int64_t getChannelLayout() { QMutexLocker(&initLocker);
-                                return audioCodec->channel_layout;
-                              }
-   AVSampleFormat getSampleFormat() { QMutexLocker(&initLocker);
-                                      return audioCodec->sample_fmt;
-                                    }
-   int getSampleRate() { QMutexLocker(&initLocker); return audioCodec->sample_rate; }
+
+   int width() { initFuture.waitForFinished(); return vCodec->width; }
+   int height() { initFuture.waitForFinished(); return vCodec->height; }
+   PixelFormat pixelFormat() { initFuture.waitForFinished(); return vCodec->pix_fmt; }
+   int64_t channelLayout() { initFuture.waitForFinished(); return aCodec->channel_layout; }
+   AVSampleFormat sampleFormat() { initFuture.waitForFinished(); return aCodec->sample_fmt; }
+   int sampleRate() { initFuture.waitForFinished(); return aCodec->sample_rate; }
 private:
    AVFormatContext* format;
-   AVCodecContext* videoCodec;
-   AVCodecContext* audioCodec;
-   int videoStreamIndex;
-   int audioStreamIndex;
+   AVCodecContext* vCodec;
+   AVCodecContext* aCodec;
+   int vIndex;
+   int aIndex;
 
-   QMutex initLocker;
+   QFuture<void> initFuture;
    void init(QString file);
    void worker();
 };
