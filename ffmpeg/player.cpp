0ecf6036db695b4059290fc506c20afb8845575c
Alexander Shabalin <shabalyn.a@gmail.com>
2012-06-29 17:13:54 +0400

    Player turned to the new interface

--- ffmpeg/player.cpp
+++ ffmpeg/player.cpp
@@ -1,54 +1,18 @@
 #include "player.h"
 
-Player::Player(int width, int height) {
-   cout << "Player is constructing with: " << width << "x" << height << endl;
-   initFuture = QtConcurrent::run(this, &Player::init, width, height);
-}
+Player::Player(QWidget *parent) { }
 
-Player::~Player() {
-   //avcodec_close(audioCodec);
-   //avformat_free_context(audioFormat);
-   //TODO: Where will it SEGFAULT?
-   //av_free(audioCodec);
-   //av_free(audioFormat);
-}
+Player::~Player() { } 
 
 void Player::newVideoFrame(QAVFrame frame) {
-   QImage img(frame.data()->data[0],frame.data()->width,frame.data()->height,QImage::Format_RGB32);
-   emit onNewFrame(QPixmap::fromImage(img));
+   image = QImage( frame.data()->data[0], frame.data()->width, frame.data()->height
+                 , QImage::Format_RGB32);
+   update();
 }
 
-void Player::newAudioFrame(QAVFrame frame) {
+void Player::newAudioFrame(QAVFrame frame) { }
 
+void Player::paintEvent(QPaintEvent*) {
+   QPainter painter(this);
+   painter.drawImage(QPointF(0,0),image);
 }
-
-void Player::init(int width, int height) {
-   //TODO: Add audio output
-   //QByteArray audioName = audioOutput.toAscii();
-
-   this->w = width;
-   this->h = height;
-}
-
-/*
-void audioCallback(void* opaque, Uint8* stream, int len) {
-   
-}
-
-void openAudio() {
-   const char* env = SDL_getenv("SDL_AUDIO_CHANNELS");
-   if(env) {
-      spec.channels = SDL_atoi(env);  
-   } else {
-      spec.channels = 2;
-   }
-   spec.format = AUDIO_S16SYS;
-   spec.samples = 1024;
-   spec.userdata = 0;
-   spec.silence = 0;
-   spec.freq = 44100;
-   spec.callback = audioCallback;
-   if(SDL_OpenAudio(&spec,0) < 0)
-      cout << "Can't open audio: " << SDL_GetError() << endl;
-}
-*/
--- ffmpeg/player.h
+++ ffmpeg/player.h
@@ -1,15 +1,12 @@
 #pragma once
 
 #include "ffmpeg.h"
-#include <QImage>
-#include <QPixmap>
 
-//TODO: It can't dynamically resized. Bad.
-class Player: public FFSink {
+class Player: public QWidget, public FFSink {
    Q_OBJECT
 public:
-   Player(int width, int height);
-   ~Player();
+   Player(QWidget *parent = 0): QWidget(parent) { }
+   ~Player() { }
 
    void newVideoFrame(QAVFrame frame);
    void newAudioFrame(QAVFrame frame);
@@ -17,18 +14,13 @@ public:
    int width() { initFuture.waitForFinished(); return w; }
    int height() { initFuture.waitForFinished(); return h; }
    PixelFormat pixelFormat() { initFuture.waitForFinished(); return PIX_FMT_RGB32; }
-   int64_t channelLayout() { initFuture.waitForFinished(); return av_get_default_channel_layout(2); }//h(spec.channels); }
+   int64_t channelLayout() { initFuture.waitForFinished();
+      return av_get_default_channel_layout(2); }//h(spec.channels); }
    AVSampleFormat sampleFormat() { initFuture.waitForFinished(); return AV_SAMPLE_FMT_S16; }
    int sampleRate() { initFuture.waitForFinished(); return 44100; }//spec.freq; }
    AVCodecContext* audioCodec() { initFuture.waitForFinished(); return 0; }
-signals:
-   void onNewFrame(QPixmap frame);
+protected:
+   void paintEvent(QPaintEvent *event);
 private:
-   //AVFormatContext* audioFormat;
-   //AVCodecContext* audioCodec;
-   QFuture<void> initFuture;
-   void init(int width, int height);
-   int w;
-   int h;
-   //SDL_AudioSpec spec;
+   QImage image;
 };
