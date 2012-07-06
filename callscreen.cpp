27d0b2de5e7603eaa73348581b823364fd35138b
Alexander Shabalin <shabalyn.a@gmail.com>
2012-07-06 19:01:43 +0400

    Somewhat working chat

--- callscreen.cpp
+++ callscreen.cpp
@@ -1,11 +1,13 @@
 #include "callscreen.h"
 #include "ui_callscreen.h"
+#include <iostream>
+using namespace std;
 
 //TODO: Add microphone into the play
 CallScreen::CallScreen( QString contactName, QString contactURI, QString localURI
                       , QWidget *parent): QDialog(parent), ui(new Ui::CallScreen) {
    ui->setupUi(this);
-   cout << "Setting up call with " << contactName << endl;
+   cout << "Setting up call with " << contactName.toAscii().data() << endl;
    this->setWindowTitle("Conversation with " + contactName);
    VideoHardware cameras;
    AudioHardware microphones;
@@ -14,22 +16,25 @@ CallScreen::CallScreen( QString contactName, QString contactURI, QString localUR
    cout << "Added default cameras/microphones" << endl;
    int camIndex = ui->comboCamera->currentIndex();
    int micIndex = ui->comboMicrophone->currentIndex();
+
    cout << "Setting up camera" << endl;
    camera = new InputGeneric( cameras.getFormats()[camIndex]
                             , cameras.getFiles()[camIndex]);
+   camera->setState(Input::Playing);
+   Input::Stream *cameraStream = camera->getStreams()[0];
+
    cout << "Setting up server to " << contactURI.toAscii().data() << endl;
    server = new OutputGeneric("mpegts", contactURI);
+   Output::Stream *serverVideoStream = server->addStream(cameraStream->info());
+   cameraStream->subscribe(serverVideoStream);
+
    cout << "Setting up remote to " << localURI.toAscii().data() << endl;
    remote = new InputGeneric("mpegts", localURI);
+   remote->setState(Input::Playing);
+   Input::Stream *remoteVideoStream = remote->getStreams()[0];
 
-   Input::Stream *cameraStream, *remoteVideoStream;
-   Output::Stream *serverVideoStream, *playerLocalVideoStream, *playerRemoteVideoStream;
-   cameraStream = camera->getStreams()[0];
-   remoteVideoStream = remote->getStreams()[0];
-   serverVideoStream = server->addStream(cameraStream->info());
-   playerLocalVideoStream = ui->playerLocal->addStream(cameraStream->info());
-   playerRemoteVideoStream = ui->playerRemote->addStream(remoteVideoStream->info());
-   cameraStream->subscribe(serverVideoStream);
+   Output::Stream *playerLocalVideoStream = ui->playerLocal->addStream(cameraStream->info());
+   Output::Stream *playerRemoteVideoStream = ui->playerRemote->addStream(remoteVideoStream->info());
    cameraStream->subscribe(playerLocalVideoStream);
    remoteVideoStream->subscribe(playerRemoteVideoStream);
 }
