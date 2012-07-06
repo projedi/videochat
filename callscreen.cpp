06e02594e70c456443a0f7d6e5271d6037d68fb7
Alexander Shabalin <shabalyn.a@gmail.com>
2012-07-06 18:49:17 +0400

    Server starts earlier the remote. Debug text to callscreen

--- callscreen.cpp
+++ callscreen.cpp
@@ -5,17 +5,22 @@
 CallScreen::CallScreen( QString contactName, QString contactURI, QString localURI
                       , QWidget *parent): QDialog(parent), ui(new Ui::CallScreen) {
    ui->setupUi(this);
+   cout << "Setting up call with " << contactName << endl;
    this->setWindowTitle("Conversation with " + contactName);
    VideoHardware cameras;
    AudioHardware microphones;
    ui->comboCamera->addItems(cameras.getNames());
    ui->comboMicrophone->addItems(microphones.getNames());
+   cout << "Added default cameras/microphones" << endl;
    int camIndex = ui->comboCamera->currentIndex();
    int micIndex = ui->comboMicrophone->currentIndex();
+   cout << "Setting up camera" << endl;
    camera = new InputGeneric( cameras.getFormats()[camIndex]
                             , cameras.getFiles()[camIndex]);
-   remote = new InputGeneric("mpegts", localURI);
+   cout << "Setting up server to " << contactURI.toAscii().data() << endl;
    server = new OutputGeneric("mpegts", contactURI);
+   cout << "Setting up remote to " << localURI.toAscii().data() << endl;
+   remote = new InputGeneric("mpegts", localURI);
 
    Input::Stream *cameraStream, *remoteVideoStream;
    Output::Stream *serverVideoStream, *playerLocalVideoStream, *playerRemoteVideoStream;
