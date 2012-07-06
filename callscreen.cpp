b1e9d7816be8c1012cb94b7c848cfa8dd6c08c77
Alexander Shabalin <shabalyn.a@gmail.com>
2012-07-06 16:42:50 +0400

    Some video logic in callscreen

--- callscreen.cpp
+++ callscreen.cpp
@@ -1,19 +1,42 @@
 #include "callscreen.h"
 #include "ui_callscreen.h"
 
-CallScreen::CallScreen(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::CallScreen)
-{
-    ui->setupUi(this);
+//TODO: Add microphone into the play
+CallScreen::CallScreen( QString contactName, QString contactURI, QString localURI
+                      , QWidget *parent): QDialog(parent), ui(new Ui::CallScreen) {
+   ui->setupUi(this);
+   this->setWindowTitle("Conversation with " + contactName);
+   VideoHardware cameras;
+   AudioHardware microphones;
+   ui->comboCamera->addItems(cameras.getNames());
+   ui->comboMicrophone->addItems(microphones.getNames());
+   int camIndex = ui->comboCamera->currentIndex();
+   int micIndex = ui->comboMicrophone->currentIndex();
+   camera = new InputGeneric( cameras.getFormats()[camIndex]
+                            , cameras.getFiles()[camIndex]);
+   remote = new InputGeneric("mpegts", localURI);
+   server = new OutputGeneric("mpegts", contactURI);
+
+   Input::Stream *cameraStream, *remoteVideoStream;
+   Output::Stream *serverVideoStream, *playerLocalVideoStream, *playerRemoteVideoStream;
+   cameraStream = camera->getStreams()[0];
+   remoteVideoStream = remote->getStreams()[0];
+   serverVideoStream = server->addStream(cameraStream->info());
+   playerLocalVideoStream = ui->playerLocal->addStream(cameraStream->info());
+   playerRemoteVideoStream = ui->playerRemote->addStream(remoteVideoStream->info());
+   cameraStream->subscribe(serverVideoStream);
+   cameraStream->subscribe(playerLocalVideoStream);
+   remoteVideoStream->subscribe(playerRemoteVideoStream);
 }
 
-CallScreen::~CallScreen()
-{
+CallScreen::~CallScreen() {
     delete ui;
+    delete remote;
+    delete camera;
+    delete microphone;
+    delete server;
 }
 
-void CallScreen::on_buttonEndCall_clicked()
-{
+void CallScreen::on_buttonEndCall_clicked() {
     this->close();
 }
--- callscreen.h
+++ callscreen.h
@@ -1,25 +1,20 @@
-#ifndef CALLSCREEN_H
-#define CALLSCREEN_H
+#pragma once
 
 #include <QDialog>
+#include "ffmpeg.h"
 
-namespace Ui {
-class CallScreen;
-}
+namespace Ui { class CallScreen; }
 
-class CallScreen : public QDialog
-{
-    Q_OBJECT
-    
+class CallScreen : public QDialog {
+Q_OBJECT
 public:
-    explicit CallScreen(QWidget *parent = 0);
-    ~CallScreen();
-    
+   explicit CallScreen( QString contactName, QString contactURI, QString localURI
+                      , QWidget *parent = 0);
+   ~CallScreen();
 private slots:
-    void on_buttonEndCall_clicked();
-
+   void on_buttonEndCall_clicked();
 private:
-    Ui::CallScreen *ui;
+   Ui::CallScreen *ui;
+   Input *remote, *camera, *microphone;
+   Output *server;
 };
-
-#endif // CALLSCREEN_H
