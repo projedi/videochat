bef71bd7e73a3c9ea73ac72eb4d29acb22f6b0b9
Alexander Shabalin <shabalyn.a@gmail.com>
2012-07-20 18:22:16 +0400

    Added scaling in RtpOutputStream

--- streaming.cpp
+++ streaming.cpp
@@ -1,7 +1,10 @@
 #include "streaming.h"
 #include <qxmpp/QXmppRtpChannel.h>
 
-RtpOutputStream::RtpOutputStream(QXmppCall* call) { this->call = call; }
+RtpOutputStream::RtpOutputStream(QXmppCall* call) {
+   this->call = call;
+   scaler = 0;
+}
 
 RtpOutputStream::~RtpOutputStream() { }
 
@@ -12,7 +15,16 @@ void RtpOutputStream::process(AVFrame* frame) {
    QXmppVideoFrame qframe( frame->linesize[0]*frame->height
                          , QSize(frame->width,frame->height), frame->linesize[0]
                          , QXmppVideoFrame::Format_YUV420P);
+   AVFrame* newFrame = avcodec_alloc_frame();
+   scaler = sws_getCachedContext( scaler, frame->width, frame->height
+                                , (PixelFormat)frame->format
+                                , frame->width, frame->height, PIX_FMT_YUV420P
+                                , SWS_BICUBIC, 0, 0, 0);
+   avpicture_alloc( (AVPicture*)newFrame, PIX_FMT_YUV420P, frame->width, frame->height);
+   sws_scale( scaler, frame->data, frame->linesize, 0, frame->height
+            , newFrame->data, newFrame->linesize);
    uchar *data = qframe.bits();
-   memcpy(data, frame->data[0], frame->linesize[0]*frame->height);
+   memcpy(data, newFrame->data[0], newFrame->linesize[0]*frame->height);
+   av_free(newFrame);
    call->videoChannel()->writeFrame(qframe);
 }
--- streaming.h
+++ streaming.h
@@ -12,4 +12,5 @@ public:
    void process(AVFrame*);
 private:
    QXmppCall* call;
+   SwsContext* scaler;
 };
