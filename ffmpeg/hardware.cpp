bd7aa15c1e53f5ad81ae7246bf927bd338b37ecc
Alexander Shabalin <shabalyn.a@gmail.com>
2012-07-27 13:11:56 +0400

    Enumerating video devices in win32

--- ffmpeg/hardware.cpp
+++ ffmpeg/hardware.cpp
@@ -3,6 +3,13 @@
 #include <iostream>
 using namespace std;
 
+#if defined(WIN32)
+#include <windows.h>
+#include <dshow.h>
+#elif defined(LINUX)
+
+#endif
+
 Hardware::~Hardware() { }
 
 QList<QString> Hardware::getNames() const { return names; }
@@ -16,6 +23,40 @@ VideoHardware::VideoHardware() {
    formats.append("v4l2");
    files.append("/dev/video1");
 #elif defined(WIN32)
+   HRESULT hr;
+   ICreateDevEnum *pSysDevEnum = NULL;
+   hr = CoCreateInstance( CLSID_SystemDeviceEnum, NULL, CLSCTX_INPROC_SERVER
+                        , IID_ICreateDevEnum, (void**)&pSysDevEnum);
+   if(FAILED(hr)) { qWarning("Can't enumerate devices"); return; }
+   IEnumMoniker *pEnumCat = NULL;
+   hr = pSysDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, &pEnumCat, 0);
+   if(hr == S_OK) {
+      IMoniker *pMoniker = NULL;
+      ULONG cFetched;
+      while(pEnumCat->Next(1, &pMoniker, &cFetched) == S_OK) {
+         IPropertyBag *pProbBag;
+         hr = pMoniker->BindToStorage(0,0,IID_IPropertyBag,(void**)&pPropBag);
+         if(SUCEEDED(hr)) {
+            VARIANT varName;
+            VariantInit(&varName);
+            varName.vt = VT_BSTR;
+            hr = propBag->Read(L"FriendlyName", &varName, 0);
+            if(SUCEEDED(hr)) {
+               names.append(QString::fromUtf8(varName.bstrVal));
+               formats.append("dshow");
+               files.append("video="+QString::fromUtf8(varName.bstrVal));
+            }
+            VariantClear(&varName);
+            pPropBag->Release();
+         }
+         pMoniker->Release();
+      }
+      pEnumCat->Release();
+   } else {
+      qWarning("Can't enumerate video devices");
+   }
+   pSysDevEnum->Release();
+
    names.append("Default webcam");
    formats.append("dshow");
    files.append("video=Venus USB2.0 Camera");
